5b1d068f0f936dc67167705ba863a82e1f2ed499
Victoooooor <victorzh716@gmail.com>
2022-02-07 03:50:58 -0500

    .

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'ErgoChairML'
 LONG_DESCRIPTION = 'Scrap Image from Google, Preprocess and generate dataset, train pix2pix'
 
