2a49a471c260138a82f66a6fac1e4ee42187773c
Victoooooor <victorzh716@gmail.com>
2022-02-07 03:12:39 -0500

    .

--- examples/test.py
+++ examples/test.py
@@ -7,21 +7,22 @@ from gen_chair import pix2pix
 
 if __name__ == "__main__":
     print(os.getcwd())
-    PATH = './pose'
+    PATH = '../data'
     path_to_zip = pathlib.Path(PATH)
     PATH = path_to_zip / 'sitting_in_chair'
-    test_dataset = tf.data.Dataset.list_files(str(PATH / 'test*.*'))
-    print(test_dataset.cardinality())
+
     cp_path = ''
-    cpdir = './checkpoints'
+    cpdir = '../checkpoints'
     p2p = pix2pix(cpdir)
     p2p.loadcp()
-
-    try:
-        test_dataset = tf.data.Dataset.list_files(str(PATH / 'test*.*'))
-    except tf.errors.InvalidArgumentError:
-        test_dataset = tf.data.Dataset.list_files(str(PATH / 'val*.*'))
-    test_dataset = test_dataset.map(p2p.load_image_test)
-    test_dataset = test_dataset.batch(p2p.BATCH_SIZE)
-    for inp, tar in test_dataset.take(2):
-      p2p.generate_images(p2p.generator, inp, tar)
+    image_names = sorted(
+        [n for n in os.listdir(PATH) if not n.startswith('.')])
+    print(image_names)
+    # try:
+    #     test_dataset = tf.data.Dataset.list_files(str(PATH / 'test*.*'))
+    # except tf.errors.InvalidArgumentError:
+    #     test_dataset = tf.data.Dataset.list_files(str(PATH / 'val*.*'))
+    # test_dataset = test_dataset.map(p2p.load_image_test)
+    # test_dataset = test_dataset.batch(p2p.BATCH_SIZE)
+    # for inp, tar in test_dataset.take(2):
+    #   p2p.generate_images(p2p.generator, inp, tar)
